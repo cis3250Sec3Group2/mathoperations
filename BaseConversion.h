eb6f6601069b8500ae1778bd10a8b81471c20cb0
Hilary Sears <hsears@uoguelph.ca>
2018-09-24 10:35:06 -0400

    pulled from branch

--- BaseConversion.h
+++ BaseConversion.h
@@ -1,6 +1,8 @@
 #ifndef BASECONVERSION_H_INCLUDED
 #define BASECONVERSION_H_INCLUDED
 
+/*Done by Hilary*/
+
 char* baseConversion(int, int, char*, int*);
 
 int willNextOverflow(unsigned long int, unsigned long int);
