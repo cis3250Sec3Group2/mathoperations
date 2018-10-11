2ec71a439dda5150f6662a38f954be473e823f9f
ZackPavel <43140378+ZackPavel@users.noreply.github.com>
2018-10-11 18:49:07 -0400

    Just now

--- PrimalityTest.c
+++ PrimalityTest.c
@@ -3,50 +3,45 @@
 #include "PrimalityTest.h"
 
 /*This functino checks to see if the passed value is a prime number*/
-int isPrime(long int num) {
-	int i;
+int isPrime( long int num ) {
+		int i;
 
 
-	if (num <= 1) {
-		return 0;
-	} else if (num <= 3) {
+		if (num <= 1) {
+					return 0;
+			} else if (num <= 3) {
+					return 1;
+			}
+
+			if (num % 2 == 0) {
+					return 0;
+			}
+
+			for (i = 3; i <= sqrt( num ); i += 2) {
+					if (num % i == 0) {
+							return 0;
+					}
+			}
+
 		return 1;
-	}
-
-
-	if (num % 2 == 0) {
-		return 0;
-	}
-
-
-	for (i = 3; i <= sqrt(num); i += 2) {
-		if (num % i == 0) {
-			return 0;
-		}
-	}
-
-	return 1;
-
 }
 
 
 /* This function gets the next prime number */
-long int getNextPrime(long int num) {
-	if (num < 2) {
-		return 2;
-	}
+long int getNextPrime( long int num ) {
+		if (num < 2) {
+				return 2;
+		}
 
+		if (num % 2 == 0) {
+				num += 1;
+		} else {
+				num += 2;
+		}
 
-	if (num % 2 == 0) {
-		num += 1;
-	} else {
-		num += 2;
-	}
+		while (isPrime(num == 0 )) {
+				num += 2;
+		}
 
-
-	while (isPrime(num == 0)) {
-		num += 2;
-	}
-
-	return num;
+		return num;
 }
